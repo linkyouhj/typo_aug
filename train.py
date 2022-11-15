68ca35b4ac6c841a45bc3f16cb42012f3b8175a1
Heejae Suh <55436953+linkyouhj@users.noreply.github.com>
2022-11-16 01:08:15 +0900

    tqdm_notebook to tqdm

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ def train_epoch(model, optimizer, loss_fn, scheduler, train_dataloader, test_dat
         train_acc = 0.0
         test_acc = 0.0
         model.train()
-        for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm_notebook(train_dataloader)):
+        for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm(train_dataloader)):
             optimizer.zero_grad()
             token_ids = token_ids.to(device)
             segment_ids = segment_ids.to(device)
@@ -28,7 +28,7 @@ def train_epoch(model, optimizer, loss_fn, scheduler, train_dataloader, test_dat
             train_acc += calc_accuracy(out, label)
         print("epoch {} train acc {}".format(e+1, train_acc / (batch_id+1)))
         model.eval()
-        for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm_notebook(test_dataloader)):
+        for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm(test_dataloader)):
             token_ids = token_ids.to(device)
             segment_ids = segment_ids.to(device)
             valid_length= valid_length
@@ -46,7 +46,7 @@ def predict(path,device,pred_dataloader,df_pred,best_epoch,augmented_size,augmen
     best_model.eval()
     test_result = []
 
-    for batch_id, (token_ids, valid_length, segment_ids) in enumerate(tqdm_notebook(pred_dataloader)):
+    for batch_id, (token_ids, valid_length, segment_ids) in enumerate(tqdm(pred_dataloader)):
         token_ids = token_ids.to(device)
         segment_ids = segment_ids.to(device)
         valid_length= valid_length
